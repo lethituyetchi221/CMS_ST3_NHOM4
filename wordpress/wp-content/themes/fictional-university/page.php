66ffcf6e86bb442cfda9931484e2bc16d5de2419
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2023-11-21 08:01:13 +0700

    update

--- wordpress/wp-content/themes/fictional-university/page.php
+++ wordpress/wp-content/themes/fictional-university/page.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+<div class="page-banner" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)">
+    <div class="page-banner__bg-image"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">Our History</h1>
         <div class="page-banner__intro">
